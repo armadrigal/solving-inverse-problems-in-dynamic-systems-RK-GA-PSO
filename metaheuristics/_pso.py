fbf07ea34635be7dbd3e4d6f52200ea07f60c780
armad-hermann <amadrigal.fismat@gmail.com>
2023-10-17 16:49:06 -0600

    se actualizo pso

--- metaheuristics/_pso.py
+++ metaheuristics/_pso.py
@@ -11,7 +11,7 @@ class PSO:
         self.interval = interval
         self._dimension = len(inspect.getargspec(self.objetive_fun).args)
 
-    def initialize_swarm(self):
+    def _initialize_swarm(self):
 
         self._positions = np.random.normal(
             self.interval[0], self.interval[1], (self.n_particles, self._dimension)
@@ -21,12 +21,12 @@ class PSO:
         )
         self._fitness = np.zeros(self.n_particles)
 
-    def calculate_fitnnes(self):
+    def _calculate_fitnnes(self):
 
         for i in range(self.n_particles):
             self._fitness[i] = self.objetive_fun(*list(self._positions[i]))
 
-    def initialize_best_positions(self):
+    def _initialize_best_positions(self):
 
         self._p_best = self._positions
         self._fitness_p_best = self._fitness
@@ -35,21 +35,21 @@ class PSO:
         self._g_best = self._positions[index_max]
         self._fitness_g_best = self._fitness[index_max]
 
-    def update_p_best(self):
+    def _update_p_best(self):
 
         for i, fitness in enumerate(self._fitness):
             if fitness > self._fitness_p_best[i]:
                 self._fitness_p_best[i] = fitness
                 self._p_best = self._positions[i]
 
-    def update_g_best(self):
+    def _update_g_best(self):
 
         index_max = np.argmax(self._fitness)
         if self._fitness[index_max] > self._fitness_g_best:
             self._fitness_g_best = self._fitness[index_max]
             self._g_best = self._positions[index_max]
 
-    def update_velocities(self):
+    def _update_velocities(self):
 
         vel = self.w*self._velocities +\
             self.c_1*np.random.rand()*(self._p_best - self._positions) +\
@@ -57,21 +57,21 @@ class PSO:
 
         self._velocities = vel
 
-    def update_positions(self):
+    def _update_positions(self):
 
         self._positions += self._velocities
 
     def fit(self, n_iterations=10):
 
-        self.initialize_swarm()
-        self.calculate_fitnnes()
-        self.initialize_best_positions()
+        self._initialize_swarm()
+        self._calculate_fitnnes()
+        self._initialize_best_positions()
 
         for i in range(1,n_iterations+1):
-            self.update_velocities()
-            self.update_positions()
-            self.calculate_fitnnes()
-            self.update_p_best()
-            self.update_g_best()
+            self._update_velocities()
+            self._update_positions()
+            self._calculate_fitnnes()
+            self._update_p_best()
+            self._update_g_best()
 
             print('Iteration: ', i, 'Gbest: ', self._g_best, 'Fitness:', self._fitness_g_best)
